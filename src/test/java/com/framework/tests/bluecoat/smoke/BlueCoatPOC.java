db3caf408b5ce5c934b445e6cee43b729b8c98aa
Suresh <suresh.babu@bluecoat.com>
2016-08-20 22:21:49 +0530

    Modified

--- src/test/java/com/framework/tests/bluecoat/smoke/BlueCoatPOC.java
+++ src/test/java/com/framework/tests/bluecoat/smoke/BlueCoatPOC.java
@@ -13,17 +13,23 @@ public class BlueCoatPOC extends BlueCoatLibrary {
 
 	@Test (description = "Widget Validation")
 	public void Test_02_Validate_Widget() throws Exception { 
-		//Both Grid and Chart
 		log("Enable Grid Only And Verify Grid Displayed");
 		bluecoatLoginPage.clickRiskSetting("Grid Only");
 		Assert.assertTrue(bluecoatLoginPage.isDisplay("bluecoatriskgrouptable"), "Risk Table Widget Is Not Enable");
 		log("Verify Graph Is Not Displayed");
 		Assert.assertFalse(bluecoatLoginPage.isDisplay("bluecoatriskgroupchartnotdisplayed"), "Risk Chart Widget Is Displayed");
+		
 		log("Enable Chart Only And Verify Chart Displayed");
 		bluecoatLoginPage.clickRiskSetting("Chart Only");
 		log("Verify Table Is Not Displayed");
 		Assert.assertFalse(bluecoatLoginPage.isDisplay("bluecoatriskgrouptable"), "Risk Table Widget Is Not Disabled");
 		log("Verify Graph is displayed");
 		Assert.assertTrue(bluecoatLoginPage.isDisplay("bluecoatriskgroupchartdisplayed"), "Risk Chart Widget Is Displayed");
+		
+		log("Enable Grid and Chart And Verify Grid And Chart Displayed");
+		bluecoatLoginPage.clickRiskSetting("Both Grid and Chart");
+		Assert.assertTrue(bluecoatLoginPage.isDisplay("bluecoatriskgrouptable"), "Risk Table Widget Is Not Enable");
+		Assert.assertTrue(bluecoatLoginPage.isDisplay("bluecoatriskgroupchartdisplayed"), "Risk Chart Widget Is Displayed");
+		
 	}
 }
